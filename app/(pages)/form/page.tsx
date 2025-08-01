da2410df430663373f2e2d8c757544aa8ae2382f
Iamsantoshpoudel <santoshpoudel06.com.np@gmail.com>
2025-08-01 19:39:32 +0545

    new features

--- app/(pages)/form/page.tsx
+++ app/(pages)/form/page.tsx
@@ -1,12 +1,23 @@
-import Form from "@/components/Form"
-
+// import Form from "@/components/Form"
+import Image from "next/image";
+import design from "@/assets/design/Untitled10.png"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 const page = () => {
   return (
-    <div>
-        <Form />
-
-      
-    </div>
+    <>
+      <Navbar />
+      <section className="flex justify-center items-center min-h-screen md:justify-start md:items-start ">
+        <Image
+          src={design}
+          alt="Placeholder image"
+          width={800}
+          height={800}
+          className="w-full h-auto max-w-full max-h-screen object-contain md:w-auto md:h-auto md:max-w-none"
+        />
+      </section>
+      <Footer />
+    </>
   )
 }
 
